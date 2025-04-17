478cb8bb6ee50069aca914cefc08bed12fe24377
chri9470 <christopher.ratheal@rackspace.com>
2025-04-17 11:10:09 -0500

    changing locals

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
-output "rds_hostname" {
-  description = "RDS instance hostname"
-  value       = module.db.db_instance_address
-}
+# output "rds_hostname" {
+#   description = "RDS instance hostname"
+#   value       = module.db.db_instance_address
+# }
 
-output "rds_port" {
-  description = "RDS instance port"
-  value       = module.db.db_instance_port
+# output "rds_port" {
+#   description = "RDS instance port"
+#   value       = module.db.db_instance_port
+# }
+output "db_engine" {
+  value = data.aws_rds_engine_version.latest.version
 }
 
 output "rds_username" {
